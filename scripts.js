5727ccb083943713af65a2c5dc3279d73b26fd3e
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-05-02 15:44:10 -0400

    starbucks plotted + zoom feature

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var geoP = d3.json("real.json")
 var stateP = d3.json("stateStuff.json")
 var headquarters = d3.csv("Fortune_Headquarter_Locations.csv")
-var starbucksP = d3.csv("Starbucks_Locations.csv")
+var starbucksP = d3.csv("new_Starbucks_Locations.csv")
 
 
 Promise.all([geoP, stateP, headquarters, starbucksP]).then(function(values){
@@ -16,7 +16,6 @@ Promise.all([geoP, stateP, headquarters, starbucksP]).then(function(values){
 // })
 
 
-
 var drawMap = function(geo, state, head, star){
   var screen = {width:1000, height: 800};
 
@@ -25,7 +24,13 @@ var svg = d3.select("svg")
             .attr("width", screen.width)
             .attr("height", screen.height);
 
-var g = svg.append("g")
+              var g = svg.append('g');
+
+            var zoom = d3.zoom()
+                  .scaleExtent([1, 8])
+                  .on('zoom', zoomed);
+
+ svg.call(zoom);
 
         var states = svg.append("g")
                       .attr("id", "states")
@@ -35,12 +40,15 @@ var g = svg.append("g")
                       .append("g")
                       .classed("state", true)
                       .on("click", function(d){
-
                         var header = d3.select("h2")
                         return header.text("State clicked: " + d.properties.NAME)
-                        // console.log("State clicked", d.properties.NAME)
+                        console.log("State clicked", d.properties.NAME)
 
                       })
+
+
+
+
 
       // states.forEach(function(d,i){
       //   console.log(d[i].NAME)
@@ -68,7 +76,23 @@ var g = svg.append("g")
             return stateGenerator(d)
           });
                // .projection(projection);
-               svg.selectAll("circle")
+               var starLocks = svg.append("g")
+                 var filteredStar = star.filter(grabUSA)
+                // console.log("Filtered Data: ",  filteredStar)
+                     starLocks.selectAll("circle")
+                      .data(filteredStar)
+                      .enter()
+                      .append("circle")
+                      .attr("cx",function(d) {return projection([d["Longitude"],d["Latitude"]])[0];})
+      .attr("cy",function(d) { return projection([d["Longitude"],d["Latitude"]])[1];})
+                      .attr("r", "2.75px")
+                        .attr("fill", "blue")
+                         .attr("opacity", ".1");
+
+
+
+              var bus = svg.append("g")
+               bus.selectAll("circle")
                	.data(head)
                	.enter()
                	.append("circle")
@@ -76,18 +100,23 @@ var g = svg.append("g")
 .attr("cy",function(d) { return projection([d["LONGITUDE"],d["LATITUDE"]])[1];})
                	.attr("r", "2.75px")
                		.attr("fill", "yellow")
-
-                  svg.selectAll("circle")
-                  	.data(star)
-                  	.enter()
-                  	.append("circle")
-                   .attr("cx",function(d) { return projection([d["Longitude"],d["Latitude"]])[0];})
-   .attr("cy",function(d) { return projection([d["Longitude"],d["Latitude"]])[1];})
-                  	.attr("r", "2.75px")
-                  		.attr("fill", "blue")
+                  .attr("opacity", "1")
+                  function grabUSA(star){
+                    return star["Country"] == "US";
+                  }
 
 
+                  function zoomed() {
+                      states
+                        .selectAll('path')
+                        .attr('transform', d3.event.transform);
+                      bus.selectAll("circle")
+                        .attr("transform", d3.event.transform)
+                      starLocks.selectAll("circle")
+                      .attr("transform", d3.event.transform)
 
+
+                    }
     //   var g = svg.append( "g" );
     //
     //   g.selectAll( "path" )
